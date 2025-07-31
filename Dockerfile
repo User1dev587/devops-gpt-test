8ce23b17551a1db09f0f267117ab115d7a38d393
User1 <User1@email.com>
2025-07-31 16:06:14 +0700

    redo

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-# Dockerfile to build NGINX image serving static website
-FROM nginx:stable
-COPY ./ /usr/share/nginx/html/
--- scripts.js
+++
@@ -1,4 +0,0 @@
-// JavaScript for the static website
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Static website loaded successfully.');
-});
